f3b0991d066dd721f51cff5d951bb340a7e2aa31
Dylan Smith <dylanfromwinnipeg@gmail.com>
2024-09-23 12:40:53 -0400

    Initial commit

---
+++ src/demo.js
@@ -0,0 +1,6 @@
+// This file also adds a new fake credential
+
+let auth_id = 'AKIAVPTPVSVXOU6WIPF4'
+let auth_token = 'QAJk/WpNmQk+tJ8r3+boU2qt0vV6AygTMlFdWzHQ'
+
+var token = "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f"
---
+++ src/index.js
@@ -0,0 +1,9 @@
+//This file introduces a fake token into a variable
+
+let auth_key = 'fb50ec73d7366c487248b80e1f97a8b2ec550b82'
+var token = "8256ba5a40014ed318f8f22cc15227b579e341ef"
+var token2 = '9278b9f396e023a0680f6100ce9371b2e7619827'
+
+var tok = "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"
+
+var new_thing = "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"
